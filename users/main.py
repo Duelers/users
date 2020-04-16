e9d04c4f3ff12de64a21d2fbfc434e52fa808da7
Murph Murphy <murph@clurictec.com>
2020-04-16 11:24:09 -0600

    Add access logging

--- users/main.py
+++ users/main.py
@@ -2,8 +2,15 @@ from aiohttp import web
 from settings import config
 from db import close_pg, init_pg
 from routes import setup_routes
+import logging
 
-app = web.Application()
+stdio_handler = logging.StreamHandler()
+stdio_handler.setLevel(logging.INFO)
+_logger = logging.getLogger('aiohttp.access')
+_logger.addHandler(stdio_handler)
+_logger.setLevel(logging.DEBUG)
+
+app = web.Application(logger=_logger)
 app['config'] = config
 app.on_startup.append(init_pg)
 app.on_cleanup.append(close_pg)
